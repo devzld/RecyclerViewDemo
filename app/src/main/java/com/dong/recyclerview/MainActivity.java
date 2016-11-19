90321540ce2fd173d9cd36c5faf55714c4c99830
devzld <779724606@qq.com>
2016-11-19 21:49:48 +0800

    test

--- app/src/main/java/com/dong/recyclerview/MainActivity.java
+++ app/src/main/java/com/dong/recyclerview/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private ArrayList<String> mDatas;
     private MyAdapter mAdapter;
-
+    //123
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
